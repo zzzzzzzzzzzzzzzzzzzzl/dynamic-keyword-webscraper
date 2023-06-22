d937110685e56417e4e33591922d751b5f08fb95
james.edwards5566@gmail.com <111021919+zzzzzzzzzzzzzzzzzzzzl@users.noreply.github.com>
2023-06-22 12:58:20 +1200

    before refatoring

--- main.py
+++ main.py
@@ -9,57 +9,61 @@ tracker = scrapedDomainTracker()
 branchDepth = 100  # how
 timeOut = 1000  # seconds
 
-csvData = readCsvFile("data/inputData.csv").pop(0)
+# csvData = readCsvFile("data/inputData.csv").pop(0)
 # driver(["https://greypower.co.nz/"], "n", 1, 20)
-if get_csv_length("data/data.csv") == 0:
-    outputData.firstLineCsv()
+# if get_csv_length("data/data.csv") == 0:
+#     outputData.firstLineCsv()
+url = ["christmastrees.co.nz"]
+
+driver(url, "test", 100, 1000).scrapeDomain()
+# def startDriver(
+#     idx,
+#     domainCompleted=True,
+# ):  # run this function in the thread
+#     while tracker.data["Incompleted"]:
+#         if domainCompleted:
+#             domain, onComplete = tracker.selectDomain()
+#             domainCompleted == False
+#             # try:
+#             driver(
+#                 ["christmastrees.co.nz"], f"thread{idx}", branchDepth, timeOut
+#             ).scrapeDomain()
+#             domainCompleted = True
+#             onComplete()
+#             tracker.saveData()
+#             # except:
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
+#             print("=========================webdriver failed=========================")
 
 
-def startDriver(
-    idx,
-    domainCompleted=True,
-):  # run this function in the thread
-    while tracker.data["Incompleted"]:
-        if domainCompleted:
-            domain, onComplete = tracker.selectDomain()
-            domainCompleted == False
-        try:
-            driver(domain, f"thread{idx}", branchDepth, timeOut).scrapeDomain()
-            domainCompleted = True
-            onComplete()
-            tracker.saveData()
-        except:
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
-            print("=========================webdriver failed=========================")
+# startDriver("test")
+# def runThreads(target, threadCount):
+#     threadArr = []
+#     if len(tracker.data["Incompleted"]) < threadCount:
+#         threadCount = len(tracker.data["Incompleted"])
+#     for i in range(threadCount):
+#         threadArr.append(threading.Thread(target=target, args=(i,)))
+
+#     for i in threadArr:
+#         i.start()
+#     for i in threadArr:
+#         i.join()
 
 
-def runThreads(target, threadCount):
-    threadArr = []
-    if len(tracker.data["Incompleted"]) < threadCount:
-        threadCount = len(tracker.data["Incompleted"])
-    for i in range(threadCount):
-        threadArr.append(threading.Thread(target=target, args=(i,)))
+# runThreads(startDriver, 25)  # main loop
+# tracker.resetInprogress()
+# runThreads(startDriver, 10)  # clean up #stuck in progress
+# tracker.resetInprogress()
+# runThreads(startDriver, 10)  # clean up #stuck in progress
+# tracker.resetInprogress()
+# runThreads(startDriver, 10)  # clean up #stuck in progress
+# tracker.resetInprogress()
+# runThreads(startDriver, 3)  # clean up #stuck in progress
+# startDriver("no threads running")
 
-    for i in threadArr:
-        i.start()
-    for i in threadArr:
-        i.join()
-
-
-runThreads(startDriver, 10)  # main loop
-tracker.resetInprogress()
-runThreads(startDriver, 10)  # clean up #stuck in progress
-tracker.resetInprogress()
-runThreads(startDriver, 10)  # clean up #stuck in progress
-tracker.resetInprogress()
-runThreads(startDriver, 10)  # clean up #stuck in progress
-tracker.resetInprogress()
-runThreads(startDriver, 3)  # clean up #stuck in progress
-startDriver("no threads running")
-
-print("Program completed.")
+# print("Program completed.")
--- scripts/driver.py
+++ scripts/driver.py
@@ -26,9 +26,181 @@ class driver:
         self.textWithKeyWords = []
         self.internalLinks = []
         self.failedToGetUrl = []
+
         self.data = data
         self.newDomain(self.link)
         self.getPageData(self.domain)
+        time.sleep(0.5)
+
+    def scrapeDomain(self):
+        self.iterateThroughInternalLinks()
+        self.close()
+        # duplicatetext = self.textWithKeyWords[:]
+        # self.textWithKeyWords = removeDuplicateFromTextWithKeyWords(
+        #     self.textWithKeyWords
+        # )
+        # if self.textWithKeyWords:
+        #     outputData(
+        #         self.textWithKeyWords,
+        #         self.domain,
+        #         self.data,
+        #         self.keywords,
+        #         duplicatetext,
+        #     )
+
+        fileManager(f"{self.idx}.json", self.textWithKeyWords).save()
+
+    def getRunTime(self):
+        if (time.time() - self.startTime) > self.timeOutAfter:
+            self.timeout = True
+
+    def getFullUrl(self, link):
+        branchFull = self.tree.addLink(link)
+        if branchFull:
+            return True
+        return self.domain + link
+
+    def newDomain(self, url):
+        self.url = url
+        print(self.url)
+
+        try:
+            self.driver.get("https://" + url)
+            self.domain = "https://" + url
+        except:
+            self.driver.get("http://" + url)
+            self.domain = "http://" + url
+
+        self.parseHtml()
+
+    def openUrl(self, url):
+        if url not in self.visitedUrls:
+            self.url = url
+
+            try:
+                self.driver.get(url)
+                self.parseHtml()
+                self.visitedUrls.append(url)
+                return True
+            except:
+                print("failed to get url")
+                self.failedToGetUrl.append(url)
+                self.visitedUrls.append(url)
+                return False
+
+    def parseHtml(self):
+        self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+    # def findTextWithKeyword(self):
+    #     textWithKeyword = {"text": [], "keywords": []}
+    #     for keyword in self.keywords:
+
+    #         def filterByKeyword(tag):
+    #             return keyword in str(tag.string).lower()
+
+    #         text = [text.string for text in self.soup.find_all(filterByKeyword)]
+    #         if text:
+    #             textWithKeyword["text"] = textWithKeyword["text"] + [
+    #                 text.string for text in text
+    #             ]
+    #             textWithKeyword["keywords"] = textWithKeyword["keywords"] + [keyword]
+
+    #     if textWithKeyword["text"]:
+    #         self.textWithKeyWords = self.textWithKeyWords + [
+    #             {
+    #                 "textsWithKeyWords": [
+    #                     textWithKeyword["text"],
+    #                     self.soup.prettify(),
+    #                 ],
+    #                 "page": self.url,
+    #             }
+    #         ]
+    def findTextWithKeyword(self):
+        textWithKeyword = {"text": [], "keywords": []}
+        for keyword in self.keywords:
+
+            def filterByKeyword(tag):
+                return keyword in str(tag.string).lower()
+
+            text = [text.string for text in self.soup.find_all(filterByKeyword)]
+            if text:
+                textWithKeyword["text"] = textWithKeyword["text"] + [
+                    text.string for text in text
+                ]
+                textWithKeyword["keywords"] = textWithKeyword["keywords"] + [keyword]
+
+        if textWithKeyword["text"]:
+            self.textWithKeyWords = self.textWithKeyWords + [
+                {
+                    "textsWithKeyWords": [
+                        textWithKeyword["text"],
+                        self.soup.prettify(),
+                    ],
+                    "page": self.url,
+                }
+            ]
+
+    def getInternalLinks(self):
+        time.sleep(0.1)
+        internalLinks = []
+        for anchor in self.soup.find_all("a"):
+            href = anchor.get("href")
+            if (
+                (not urlparse(href).netloc or self.domain in href)
+                and href
+                and " " not in href
+                and "@" not in href
+            ):
+                url = self.getFullUrl(href)
+                if url == True:
+                    pass
+                elif url not in self.internalLinks:
+                    if "https" in url[4:]:
+                        a, b = chopchop(url)
+                        internalLinks.append(a)
+                        internalLinks.append(b)
+                    else:
+                        internalLinks.append(url)
+
+        self.internalLinks = self.internalLinks + internalLinks
+
+    def getPageData(self, url):
+        goturl = self.openUrl(url)
+        if goturl:
+            self.findTextWithKeyword()
+            self.getInternalLinks()
+            return goturl
+
+    def iterateThroughInternalLinks(self, recusions=0):
+        time.sleep(0.5)
+        self.getRunTime()
+        if self.timeout:
+            print(
+                f"timeout",
+            )
+            return
+        else:
+            recusions += 1
+            while self.internalLinks and not self.timeout:
+                if self.internalLinks[0] not in self.visitedUrls:
+                    goturl = self.getPageData(self.internalLinks[0])
+                    if goturl:
+                        print(
+                            "idx  :",
+                            self.internalLinks[0],
+                            "recursion  :",
+                            recusions,
+                            "links :",
+                            len(self.internalLinks),
+                        )
+                        self.iterateThroughInternalLinks(recusions)
+                try:
+                    self.internalLinks.pop(0)
+                except:
+                    print("list probably empty")
+
+    def close(self):
+        self.driver.quit()
 
     def configDriver(self):
         options = webdriver.ChromeOptions()
@@ -54,147 +226,3 @@ class driver:
         driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": userAgent})
 
         return driver
-
-    def scrapeDomain(self):
-        self.iterateThroughInternalLinks()
-        self.close()
-        duplicatetext = self.textWithKeyWords[:]
-        self.textWithKeyWords = removeDuplicateFromTextWithKeyWords(
-            self.textWithKeyWords
-        )
-        if self.textWithKeyWords:
-            outputData(
-                self.textWithKeyWords,
-                self.domain,
-                self.data,
-                self.keywords,
-                duplicatetext,
-            )
-        # fileManager(f"{self.idx}.json", self.textWithKeyWords).save()
-
-    def getRunTime(self):
-        if (time.time() - self.startTime) > self.timeOutAfter:
-            self.timeout = True
-
-    def getFullUrl(self, link):
-        branchFull = self.tree.addLink(link)
-        if branchFull:
-            return True
-        return self.domain + link
-
-    def newDomain(self, url):
-        self.url = url
-        print(self.url)
-
-        try:
-            self.driver.get("https://" + url)
-            self.domain = "https://" + url
-        except:
-            self.driver.get("https://" + url)
-            self.domain = "https://" + url
-
-        self.parseHtml()
-
-    def openUrl(self, url):
-        if url not in self.visitedUrls:
-            self.url = url
-
-            try:
-                self.driver.get(url)
-                self.parseHtml()
-                self.visitedUrls.append(url)
-                return True
-            except:
-                print("failed to get url")
-                self.failedToGetUrl.append(url)
-                self.visitedUrls.append(url)
-                return False
-
-    def parseHtml(self):
-        self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
-
-    def findTextWithKeyword(self):
-        textWithKeyword = {"text": [], "keywords": []}
-        for keyword in self.keywords:
-
-            def filterByKeyword(tag):
-                return (
-                    tag.name != "script"
-                    and tag.name != "style"
-                    and tag.string
-                    and keyword in tag.string
-                )
-
-            text = [text.string for text in self.soup.find_all(filterByKeyword)]
-            if text:
-                textWithKeyword["text"] = textWithKeyword["text"] + [
-                    text.string for text in text
-                ]
-                textWithKeyword["keywords"] = textWithKeyword["keywords"] + [keyword]
-
-        if textWithKeyword["text"]:
-            self.textWithKeyWords = self.textWithKeyWords + [
-                {"textsWithKeyWords": textWithKeyword, "page": self.url}
-            ]
-
-    def getInternalLinks(self):
-        time.sleep(0.1)
-        internalLinks = []
-        for anchor in self.soup.find_all("a"):
-            href = anchor.get("href")
-            if (
-                (not urlparse(href).netloc or self.domain in href)
-                and href
-                and " " not in href
-                and "@" not in href
-                # and "www." not in href
-            ):
-                url = self.getFullUrl(href)
-                if url == True:
-                    pass
-                elif url not in self.internalLinks:
-                    if "https" in url[4:]:
-                        a, b = chopchop(url)
-                        internalLinks.append(a)
-                        internalLinks.append(b)
-                    else:
-                        internalLinks.append(url)
-
-        self.internalLinks = self.internalLinks + internalLinks
-
-    def getPageData(self, url):
-        goturl = self.openUrl(url)
-        if goturl:
-            self.findTextWithKeyword()
-            self.getInternalLinks()
-            return goturl
-
-    def iterateThroughInternalLinks(self, recusions=0):
-        self.getRunTime()
-        if self.timeout:
-            print(
-                f"timeout",
-            )
-            return
-        else:
-            recusions += 1
-            while self.internalLinks and not self.timeout:
-                if self.internalLinks[0] not in self.visitedUrls:
-                    goturl = self.getPageData(self.internalLinks[0])
-                    if goturl:
-                        print(
-                            "idx  :",
-                            self.domain,
-                            "recursion  :",
-                            recusions,
-                            "links :",
-                            len(self.internalLinks),
-                        )
-                        self.iterateThroughInternalLinks(recusions)
-                try:
-                    self.internalLinks.pop(0)
-                except:
-                    print("list probably empty")
-
-    def close(self):
-        self.driver.quit()
